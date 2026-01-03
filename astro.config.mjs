28fff90b0b6318740890938a12660c14e51dbc62
David <davidrossaudio@gmail.com>
2026-01-04 10:20:43 +1100

    astro github build config -- /workflows/deploy.yaml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+    integrations: [react()],
+    site: "https://davidross.xyz",
+    base: "/",
 });
+
+// URL bsaed on github pages -- http://davidross.xyz/
+// Your site is live at http://davidross.xyz/
