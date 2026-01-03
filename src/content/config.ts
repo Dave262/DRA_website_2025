216ff3c251e45ba883cbf41586f27e8c8f037080
David Ross <davidrossaudio@gmail.com>
2026-01-03 13:29:22 +1100

    organised index.html into separate modules and added new video to home page - improved blog and deleted some uneccesary files - round 2 -- added the actual files

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,24 +1,20 @@
 import { defineCollection, z } from "astro:content";
 
-
 const blogCollection = defineCollection({
-
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    pubDate: z.date(),
-    author: z.string(),
-    description: z.string(),
-    image: z.object({
-      url: z.string(),
-      alt: z.string(),
+    type: "content",
+    schema: z.object({
+        title: z.string(),
+        pubDate: z.date(),
+        author: z.string(),
+        description: z.string(),
+        image: z.object({
+            url: z.string(),
+            alt: z.string(),
+        }),
+        tags: z.array(z.string()),
     }),
-    tags: z.array(z.string()),
-
-  })
 });
 
 export const collections = {
-  blog: blogCollection,
-
+    blog: blogCollection,
 };
